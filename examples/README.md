e4ce282836b666579b4a627a27bc95b62f8fa659
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-05-13 15:02:53 -0700

    add typescript example

---
+++ examples/README.md
@@ -0,0 +1,7 @@
+# saucectl puppeteer examples
+
+Here we have a list additional examples that cover more complicated setups.
+
+| Example | Description |
+| ------- | ----------- |
+| [typescript](typescript) | Showcases how to run tests using puppeteer with typescript |
---
+++ examples/typescript/tests/example.test.ts
@@ -0,0 +1,9 @@
+describe('saucectl demo test', () => {
+    test('should verify title of the page', async function () {
+        const host:string = "https://www.saucedemo.com/";
+        const expected:string = "Swag Labs"
+        const page = (await browser.pages())[0]
+        await page.goto(host);
+        expect(await page.title()).toBe(expected);
+    });
+});
