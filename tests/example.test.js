954a9f47b505f27a9fa36f597c1c984eec44021c
nadvolod <nadvolod@gmail.com>
2021-08-31 14:13:39 -0400

    add another test

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,6 +1,11 @@
 describe('saucectl demo test', () => {
 	test('should verify title of the page', async () => {
-		const page = (await browser.pages())[0]
+		const page = await browser.newPage();
+		await page.goto('https://www.saucedemo.com/');
+		expect(await page.title()).toBe('Swag Labs');
+	});
+	test('should login with valid user', async () => {
+		const page = await browser.newPage();
 		await page.goto('https://www.saucedemo.com/');
 		expect(await page.title()).toBe('Swag Labs');
 	});
