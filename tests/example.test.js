758a84ac0debb67315d60a48bb218668a2ad3a24
nadvolod <nadvolod@gmail.com>
2021-09-01 14:18:15 -0400

    add a login test

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,11 +1,5 @@
-describe('saucectl demo test', () => {
+describe('saucectl simple test', () => {
 	test('should verify title of the page', async () => {
-		const page = await browser.newPage();
-		await page.goto('https://www.saucedemo.com/');
-		expect(await page.title()).toBe('Swag Labs');
-	});
-
-	test('should verify title of the page 3', async () => {
 		const page = await browser.newPage();
 		await page.goto('https://www.saucedemo.com/');
 		expect(await page.title()).toBe('Swag Labs');
--- tests/example2.test.js
+++
@@ -1,7 +0,0 @@
-describe('saucectl demo test 2', () => {
-	test('should verify title of the page 2', async () => {
-		const page = await browser.newPage();
-		await page.goto('https://www.saucedemo.com/');
-		expect(await page.title()).toBe('Swag Labs');
-	});
-});
