afc43ba51b3f8e55656ac3cbf6d0b0042c12ebd2
Tian Feng <tian.feng@saucelabs.com>
2021-09-10 10:15:29 -0700

    [DEVX-1208] Add example for running by group (#14)

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,4 +1,7 @@
-describe('saucectl simple test', () => {
+/*
+ * @group group1
+ */
+describe('saucectl simple test group1', () => {
 	test('should verify title of the page', async () => {
 		const page = await browser.newPage();
 		await page.goto('https://www.saucedemo.com/');
